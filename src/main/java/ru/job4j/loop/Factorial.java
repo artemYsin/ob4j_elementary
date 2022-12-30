649e6747fc932026c82a6e9f183bd6dae730800b
artemYusin <ysin.artem@gmail.com>
2022-12-30 14:53:38 +0300

    5.2. Создать программу, вычисляющую факториал. [#193]

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int result = 0;
+        if (n == 0) result = 1;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
