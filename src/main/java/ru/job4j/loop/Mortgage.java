d80d4f7aeb82829106c7292b39ea48e97d644a46
artemYusin <ysin.artem@gmail.com>
2023-01-02 09:06:17 +0300

    5.7. Ипотека [#156315 #376492]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double totalAmount = amount * percent / 100 + amount;
-        while (totalAmount > 0) {
-            totalAmount -= salary;
+        while (amount > 0) {
+            amount = (amount * percent / 100 + amount) - salary;
             year++;
         }
         return year;
