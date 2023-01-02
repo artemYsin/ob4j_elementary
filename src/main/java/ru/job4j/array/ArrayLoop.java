d35edb1fe82a826e715926fcddd40829278a6663
artemYusin <ysin.artem@gmail.com>
2023-01-02 12:13:34 +0300

    6.0.5. Массивы и цикл for-each [#333581 #376673]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -2,11 +2,11 @@ package ru.job4j.array;
 
 public class ArrayLoop {
     public static void main(String[] args) {
-        int[] massive = new int[5];
-        for (int i = 0; i < massive.length; i++) {
-            massive[i] = i * 2 + 3;
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * 2 + 3;
         }
-        for (int number : massive) {
+        for (int number : array) {
             System.out.println(number);
         }
     }
